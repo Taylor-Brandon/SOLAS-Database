826ffe1fcb258b880092eeffae7599d4f6f620b2
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-03-11 21:14:50 -0400

    added seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,14 @@
 const db = require("../config/connection");
-const { User } = require("../models");
+const { User, SOLAS } = require("../models");
 const userData = require("./userData.json");
+const solasData = require('./solasData.json');
 
 db.once("open", async () => {
   try {
     await User.deleteMany({});
+    await SOLAS.deleteMany({});
     await User.insertMany(userData);
+    await SOLAS.insertMany(solasData);
 
     console.log("all done!");
     process.exit(0);
