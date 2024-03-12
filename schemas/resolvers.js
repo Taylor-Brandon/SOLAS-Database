5439307f54f61fe7d0204ea102b53a919de5345a
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-03-11 21:16:54 -0400

    added resolver data

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,10 +1,13 @@
-const { User } = require("../models");
+const { User, SOLAS } = require("../models");
 
 const resolvers = {
   Query: {
     users: async () => {
       return await User.find({});
     },
+    solas: async() => {
+      return await SOLAS.find({});
+    }
   },
 };
 
